074b3795b6a83aafe300f086023a3745c19ea1d2
AHmims <ali.hmims99@gmail.com>
2021-05-09 22:52:36 +0000

    get all products with categories

--- app/Repository/Product/ProductRepository.php
+++ app/Repository/Product/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository\Product;
 
 use App\Models\Product;
+use App\Repository\Category\CategoryRepositoryInterface;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -10,6 +11,13 @@ use Illuminate\Http\Request;
 
 class ProductRepository implements ProductRepositoryInterface
 {
+    private $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
     //Get all products
     public function getAll(): Collection
     {
@@ -19,17 +27,52 @@ class ProductRepository implements ProductRepositoryInterface
     public function store(Request $request)
     {
         try {
-            $product = new Product();
-            // return $request->name;
-            $product->name = $request->name;
-            $product->description = $request->description;
-            $product->price = $request->price;
-            $product->image = $request->image;
-            // $product->categories()->save()
+            $product = $this->toProduct($request);
+            if ($product == null)
+                throw new Exception("Error Processing Request");
+            //
+
+            $categories = array();
+            for ($i = 0; $i < count($request->categories); $i++) {
+                $categories[$i] = $this->categoryRepository->get($request->categories[$i]);
+            }
+
             $product->save();
+            $product->categories()->saveMany($categories);
             return "good";
         } catch (Exception $e) {
             return $e->getMessage();
+        }
+    }
+    //
+    private function toProduct(Request $request)
+    {
+        try {
+            $product = new Product();
+            //
+            if ($request->name == null || strlen($request->name) <= 0)
+                throw new Exception("Bad name");
+            $product->name = $request->name;
+            //
+            if ($request->description == null || strlen($request->description) <= 0)
+                throw new Exception("Bad description");
+            $product->description = $request->description;
+            //
+            if ($request->price == null || $request->price <= 0)
+                throw new Exception("Bad price");
+            $product->price = $request->price;
+            //
+            if ($request->image == null || strlen($request->image) <= 0)
+                throw new Exception("Bad image");
+            $product->image = $request->image;
+            //
+            if (count($request->categories) == 0)
+                throw new Exception("No category");
+
+            //
+            return $product;
+        } catch (Exception $e) {
+            return null;
         }
     }
 }
