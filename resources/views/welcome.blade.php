8363419b0813ef33ac61d9e288cfefd43211e661
AHmims <ali.hmims99@gmail.com>
2021-05-15 17:31:33 +0000

    css ref on hosted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Laravel</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <title>Products management app</title>
 </head>
 
 <body class="antialiased h-screen">
